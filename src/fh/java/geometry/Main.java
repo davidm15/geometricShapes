b4e61e53d3c23eafd181c55f5e5e715de1e550b8
davidm15 <david.mischak@edu.fh-joanneum.at>
2016-11-04 16:38:21 +0100

    This was Exercise2

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -10,18 +10,73 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-		//TODO: main() is missing
+    public static void main(String[] args) {
 
-
-    public void fillList(List<GeometricShape> shapeList) {
-		//TODO: Implement
-
+        Main main = new Main();
+        main.doIt();
     }
 
+    private void doIt() {
+
+        List<GeometricShape> shapeList = new ArrayList<>();
+
+        fillList(shapeList);
+
+        printList(shapeList);
+
+        List<Circle> circleList = new ArrayList<>();
+        for (GeometricShape shape : shapeList) {
+            if (shape instanceof Circle) {
+                //circleList.add((Circle)shape);
+            }
+        }
+        System.out.println("------ circle ------");
+        for (Circle circle : circleList) {
+            System.out.println(circle);
+        }
+
+    }
 
     public void printList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        System.out.println("------ for loop ------");
+        for (int pos = 0; pos < shapeList.size(); pos++) {
+            System.out.println(shapeList.get(pos));
+        }
+        // Never do that again above!
+
+        System.out.println("------ for each ------");
+        for (GeometricShape shape : shapeList) {
+
+            System.out.println(shape);
+        }
+
+        System.out.println("------ stream 1 ------");
+        shapeList
+                .stream()
+                .forEach(shape-> System.out.println(shape));
+
+        System.out.println("------ stream 2 ------");
+        shapeList
+                .stream()
+                .forEach(System.out::println);
 
     }
+
+    public void fillList(List<GeometricShape> shapeList) {
+
+        shapeList.add(new Circle(1,2,2.0));
+        shapeList.add(new Circle(1,3,2.0));
+        shapeList.add(new Circle(4,2,5.0));
+
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+        shapeList.add(new RightTriangle(40,22,10.0,11.0));
+        shapeList.add(new RightTriangle(11,40,10.0,11.0));
+
+        shapeList.add(new Rectangle(1,60,20.1,21.11));
+        shapeList.add(new Rectangle(1,2,20.1,21.11));
+        shapeList.add(new Rectangle(35,2,20.1,21.11));
+    }
+
 
 }
